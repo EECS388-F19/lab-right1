bba3b286169d8be60126e08dcadbf09b30a50704
valagapp <valagapp@umich.edu>
2019-09-09 17:45:02 -0400

    Random sum helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+from random import randint
+print("Viknesh")
+x = randint(0,100)
+y = randint(0,100)
+sum = x + y
+avg = sum/2.0
+print(x)
+print(y)
+print("Sum = " + str(sum))
+print("Average = " + str(avg))
